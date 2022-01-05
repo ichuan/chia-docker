54941f549ffa7c4f56c5a6e49f4a4fc4cf7de748
ichuan <iyanchuan@gmail.com>
2022-01-05 16:17:35 +0800

    fix: update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 # listen IP
 sed -i 's/self_hostname: 127.0.0.1/self_hostname: 0.0.0.0/g' /root/.chia/mainnet/config/config.yaml
 
-exec bash /chia-blockchain/entrypoint.sh
+# see https://github.com/Chia-Network/chia-docker/blob/main/Dockerfile
+exec /usr/local/bin/docker-entrypoint.sh docker-start.sh
